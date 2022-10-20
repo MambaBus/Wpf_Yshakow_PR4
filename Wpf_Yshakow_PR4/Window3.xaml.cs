4b4baa5f6ff5b38ab1e552af9d9abfe5d2a828b2
mambabusin142@mail.ru <mambabusin142@mail.ru>
2022-10-20 15:30:20 +1000

    Пример 3

--- Wpf_Yshakow_PR4/Window3.xaml.cs
+++ Wpf_Yshakow_PR4/Window3.xaml.cs
@@ -26,22 +26,26 @@ namespace Wpf_Yshakow_PR4
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
-
+            Window4 window4 = new Window4();
+            window4.Show();
         }
 
         private void Button_Click_1(object sender, RoutedEventArgs e)
         {
-
+            Window5 window5 = new Window5();
+            window5.Show();
         }
 
         private void Button_Click_2(object sender, RoutedEventArgs e)
         {
-
+            Window6 window6 = new Window6();
+            window6.Show();
         }
 
         private void Button_Click_3(object sender, RoutedEventArgs e)
         {
-
+            Window7 window7 = new Window7();
+            window7.Show();
         }
     }
 }
